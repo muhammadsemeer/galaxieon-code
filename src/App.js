bb95c5bb265375df49fc8d43c3a6ccaa3703b527
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-05-25 18:34:24 +0530

    Inital Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss"
+import logo from "./logo.svg"
+const App = () => {
+  return (
+    <div className="App">
+      <img src={logo} alt="logo" />
+      <h1>Galaxieon Code</h1>
+      <p>Code Editor Super Powered By <a href="https://galaxieon.com">Galaxieon</a></p>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  entry: "src/index.js",
+  output: {
+    filename: "chunk.[fullhash].js",
+    path: path.resolve(__dirname, "build"),
+    assetModuleFilename: "assets/images/[hash][ext][query]",
+  },
+  resolve: {
+    modules: [__dirname, "src", "node_modules"],
+    extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: require.resolve("babel-loader"),
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      favicon: "./public/favicon.ico",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "./public/",
+          to: "./",
+          globOptions: {
+            dot: true,
+            gitignore: true,
+            ignore: ["**/*.html", "**/*.js", "**/*.css", "**/*.scss"],
+          },
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: "vendor.[contenthash].css",
+    }),
+    new Dotenv(),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, `src`),
+    port: 3000,
+    open: true,
+    hot: true,
+    historyApiFallback: true,
+  },
+};
