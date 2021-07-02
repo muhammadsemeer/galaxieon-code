8c89f2f8559ab0b3b3913c5b4cddaad5949896bf
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-07-02 23:48:31 +0530

    navbar and sidebar modifed

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -20,7 +20,13 @@ const SideBar: FC = () => {
 
   return (
     <Menu
-      style={{ width: 256, height: "calc(100vh - 50px)", padding: "25px 0" }}
+      style={{
+        width: 256,
+        height: "calc(100vh - 50px)",
+        padding: "25px 0",
+        position: "fixed",
+        marginTop: "65px",
+      }}
       defaultSelectedKeys={[keys[location.pathname]]}
       mode="inline"
     >
