5578baf332cfe706e250c302832044295814997f
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-07-04 16:37:52 +0530

    created adminRouter

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -2,16 +2,19 @@ import React, { FC } from "react";
 import { Route, Switch } from "react-router-dom";
 import AdminDash from "./AdminDash";
 import Login from "./AdminLogin";
+import AdminRoute from "../components/Private/AdminRoute"
 
 const Admin: FC = () => {
   return (
     <>
-      <Route path="/admin/login">
-        <Login />
-      </Route>
-      <Route exact path="/admin">
-        <AdminDash />
-      </Route>
+      <Switch>
+        <Route path="/admin/login">
+          <Login />
+        </Route>
+        <AdminRoute exact path="/admin">
+          <AdminDash />
+        </AdminRoute>
+      </Switch>
     </>
   );
 };
