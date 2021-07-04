f5cb8597820f2983e71b370c5827b1c2fe5af506
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-07-04 12:36:19 +0530

    admin login api intergated and error handling

--- src/pages/AdminLogin.tsx
+++ src/pages/AdminLogin.tsx
@@ -1,36 +1,62 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import styles from "../styles/login.module.scss";
-import { Card, Typography, Divider } from "antd";
+import { Card, Typography, Divider, Result, Button, ResultProps } from "antd";
 import GoogleLogin from "react-google-login";
-import LoginGithub from "react-login-github";
+import axois from "../api/index";
+import { AxiosError, AxiosResponse } from "axios";
 
 const { Title, Paragraph, Text } = Typography;
 
 const Login: FC = () => {
+  const [error, setError] = useState<{ status: any; message: string }>();
+
   const handleSuccess = (resp: any) => {
     let token = resp.tokenId;
-    console.log(token);
+    axois
+      .post("/auth/admin", { token })
+      .then((res: AxiosResponse) => {
+        console.log(res.data);
+      })
+      .catch(({ response }: AxiosError) =>
+        setError({
+          status: response?.status.toString(),
+          message: response?.data,
+        })
+      );
   };
-  const handleFailure = (error: Error) => {
-    console.log(error);
+  const handleFailure = ({ error }: any) => {
+    setError({ status: "error", message: error });
   };
 
   return (
     <div className={styles.container}>
-      <Card className={styles.card}>
-        <Title level={2}>Galaxieon Code</Title>
-        <Paragraph>
-          Online Code Editor Super Powered By <Text strong> Galaxieon </Text>
-        </Paragraph>
-        <Divider />
-        <GoogleLogin
-          clientId={process.env.GOOGLE_CLIENT_ID as string}
-          buttonText="Login With Google"
-          accessType="online"
-          onSuccess={handleSuccess}
-          onFailure={handleFailure}
+      {!error ? (
+        <Card className={styles.card}>
+          <Title level={2}>Galaxieon Code</Title>
+          <Paragraph>
+            Online Code Editor Super Powered By <Text strong> Galaxieon </Text>
+          </Paragraph>
+          <Divider />
+          <GoogleLogin
+            clientId={process.env.GOOGLE_CLIENT_ID as string}
+            buttonText="Login With Google"
+            accessType="online"
+            onSuccess={handleSuccess}
+            onFailure={handleFailure}
+          />
+        </Card>
+      ) : (
+        <Result
+          status={error.status}
+          title={error.status}
+          subTitle={error.message}
+          extra={
+            <Button type="primary" onClick={() => setError(undefined)}>
+              Try Again
+            </Button>
+          }
         />
-      </Card>
+      )}
     </div>
   );
 };
