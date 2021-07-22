e7ea6447c2c8a4d32d97540331182d37ee9b8610
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-07-22 12:34:48 +0530

    User private route created

---
+++ src/components/Private/UserRoute.tsx
@@ -0,0 +1,21 @@
+import React, { FC, PropsWithChildren } from "react";
+import { Route, RouteProps, Redirect } from "react-router-dom";
+import { RootState } from "../../store/index";
+import { useSelector } from "react-redux";
+
+const AdminRoute: FC<PropsWithChildren<RouteProps>> = ({
+  children,
+  ...rest
+}) => {
+  const auth = useSelector((state: RootState) => state.auth);
+  return (
+    <Route
+      {...rest}
+      render={() =>
+        auth.login && auth.user ? children : <Redirect to="/login" />
+      }
+    />
+  );
+};
+
+export default AdminRoute;
