70c07274c09f21517c576dbffbdcf0b7035d4a02
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-07-23 14:22:23 +0530

    sending menus details as props

--- src/components/NavBar/Admin.tsx
+++ src/components/NavBar/Admin.tsx
@@ -1,15 +1,52 @@
 import React, { FC } from "react";
-import SideBar from "../SideBar/SideBar";
+import SideBar, { SideBarProps } from "../SideBar/SideBar";
 import Logo from "./Logo";
 import Profile from "./Profile";
+import {
+  DashboardOutlined,
+  CloudServerOutlined,
+  FileOutlined,
+  TeamOutlined,
+} from "@ant-design/icons";
+
 const Admin = () => {
+  const sidebarProps: SideBarProps = {
+    isAdmin: true,
+    menus: [
+      {
+        name: "Dashboard",
+        key: "dashboard",
+        icon: <DashboardOutlined />,
+        to: "/admin",
+      },
+      {
+        name: "Users",
+        key: "users",
+        icon: <TeamOutlined />,
+        to: "/admin/users",
+      },
+      {
+        name: "Templates",
+        key: "templates",
+        icon: <FileOutlined />,
+        to: "/admin/templates",
+      },
+      {
+        name: "Instances",
+        key: "instances",
+        icon: <CloudServerOutlined />,
+        to: "/admin/instances",
+      },
+    ],
+  };
+
   return (
     <>
       <header>
         <Logo isNav />
         <Profile isAdmin />
       </header>
-      <SideBar />
+      <SideBar {...sidebarProps} />
     </>
   );
 };
