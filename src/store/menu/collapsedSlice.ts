25bfa4c1a3543fce23de9fa91f752f0673b94c80
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-07-23 13:10:06 +0530

    collapsedSlice initalvalue modifed and added new reducer

--- src/store/menu/collapsedSlice.ts
+++ src/store/menu/collapsedSlice.ts
@@ -2,14 +2,17 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 const collapsedSlice = createSlice({
   name: "collapsed",
-  initialState: false,
+  initialState: window.innerWidth <= 980,
   reducers: {
     collapse: (state) => {
       return !state;
     },
+    collapseWithPayload: (state, payload: PayloadAction<boolean>) => {
+      return payload.payload;
+    },
   },
 });
 
-export const { collapse } = collapsedSlice.actions;
+export const { collapse, collapseWithPayload } = collapsedSlice.actions;
 
 export default collapsedSlice.reducer;
