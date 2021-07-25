de8a49ebd082b8973746d694ca600152ef4adbfd
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-07-25 17:49:45 +0530

    CardLoading component

---
+++ src/components/Card/CardLoading.tsx
@@ -0,0 +1,17 @@
+import { Col, Row } from "antd";
+import React, { FC } from "react";
+import Card from "./Card";
+
+const CardLoading: FC<{ count: number }> = ({ count }) => {
+  const arr = [];
+  for (let i = 0; i < count; i++) {
+    arr.push(
+      <Col key={i} sm={24} md={12} lg={8} xl={6} xxl={3}>
+        <Card style={{ width: "100%" }} cardId={i.toString()} content={{}} loading></Card>
+      </Col>
+    );
+  }
+  return <>{arr}</>;
+};
+
+export default CardLoading;
