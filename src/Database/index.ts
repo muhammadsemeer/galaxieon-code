67110934c9fe12c43a89b0ab152d934fdf949116
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-08-05 21:52:32 +0530

    db updated and add funtion added

--- src/Database/index.ts
+++ src/Database/index.ts
@@ -11,21 +11,32 @@ class Database {
     return new Promise((resolve, reject) => {
       let request = indexedDB.open(this.dbName, this.version);
       request.addEventListener("success", (event) => {
+        this.db = request.result;
         resolve(true);
       });
       request.addEventListener("error", (event) => {
         reject(request.error);
       });
       request.addEventListener("upgradeneeded", (event) => {
-        this.db = request.result;
         stores.forEach(({ name, options }) => {
-          this.db.createObjectStore(name, options);
+          request.result.createObjectStore(name, options);
         });
       });
     });
   }
   delete(): void {
     indexedDB.deleteDatabase(this.dbName);
+  }
+  add(objectStoreName: string, key: string, value: any): Promise<Event> {
+    return new Promise((resolve, reject) => {
+      console.log({ objectStoreName, key, value });
+      let request = this.db
+        .transaction(objectStoreName, "readwrite")
+        .objectStore(objectStoreName)
+        .add({value, key});
+      request.addEventListener("success", resolve);
+      request.addEventListener("error", reject);
+    });
   }
 }
 
