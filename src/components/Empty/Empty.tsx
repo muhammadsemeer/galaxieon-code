c784343ef1813c9ef853e498ad9c01fc0b21a2dd
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-10-22 23:26:22 +0530

    add action to epmty create button

--- src/components/Empty/Empty.tsx
+++ src/components/Empty/Empty.tsx
@@ -1,5 +1,6 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { Button, Empty as AntEmpty } from "antd";
+import CreateModal from "../CreateModal/CreateModal";
 
 export interface EmptyProps {
   height?: number | string;
@@ -7,13 +8,22 @@ export interface EmptyProps {
 }
 
 const Empty: FC<EmptyProps> = ({ height, create }) => {
+  const [isModalUp, setModalUp] = useState(false);
   return (
     <div
       className="flex middle"
       style={{ height: height ? height : "calc(100vh - 300px)" }}
     >
       <AntEmpty image={AntEmpty.PRESENTED_IMAGE_SIMPLE}>
-        {create && <Button type="primary">Create New Instance</Button>}
+        {create && (
+          <>
+            <Button onClick={() => setModalUp(true)} type="primary">Create New Instance</Button>
+            <CreateModal
+              visible={isModalUp}
+              onCancel={() => setModalUp(false)}
+            />
+          </>
+        )}
       </AntEmpty>
     </div>
   );
