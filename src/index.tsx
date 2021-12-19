f1d42f395c661929af3b87411dc970b0e2574add
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-12-19 12:09:30 +0530

    sentry error monitoring

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import store from "./store/index";
@@ -9,14 +11,22 @@ import "antd/dist/antd.dark.css";
 import "./fonts/fonts.scss";
 import "./index.scss";
 
+if (process.env.NODE_ENV !== "development") {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: Number(process.env.SENTRY_TRACES_SAMPLE_RATE),
+    environment: process.env.NODE_ENV,
+  });
+}
+
 ReactDOM.render(
   // <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   // </React.StrictMode>
-  ,
   document.getElementById("root")
 );
