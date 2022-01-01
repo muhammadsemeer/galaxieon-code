07e770198d73e3eed4bc5cf0f32f07b70cc8ab53
muhammadsemeer <muhammadsemeerali@gmail.com>
2022-01-01 20:56:50 +0530

    Added stats in admin

--- src/pages/AdminDash.tsx
+++ src/pages/AdminDash.tsx
@@ -1,9 +1,13 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { Row, Col, Space } from "antd";
 import StatCards, { Stats } from "../components/StatCards/StatCards";
 import { CodeOutlined, UserOutlined, FileOutlined } from "@ant-design/icons";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../store";
+import axios from "../api/index";
+import { AxiosResponse } from "axios";
+import handleError from "../utils/Error";
+import { useHistory } from "react-router-dom";
 
 let dummyData = [
   { title: "Users", value: 100 },
@@ -17,6 +21,20 @@ const AdminDash: FC = () => {
   const [stats, setStats] = useState<Stats[]>(dummyData);
 
   const collapsed = useSelector((state: RootState) => state.collapsed);
+
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    axios
+      .get("/admin/stats")
+      .then((res: AxiosResponse<Stats[]>) => {
+        setStats(res.data);
+      })
+      .catch((err) => {
+        handleError(err, history, dispatch, true);
+      });
+  }, []);
 
   return (
     <main className={`admin p-left ${collapsed && "collapsed"}`}>
