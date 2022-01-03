ca2eef33cc3c63eaac7f2f4bc5cb568919954057
muhammadsemeer <muhammadsemeerali@gmail.com>
2022-01-03 12:51:47 +0530

    redirection to https://galaxieon.com/gcode.html when screen < 1024

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import NavBar from "./components/NavBar/Index";
 import Routes from "./pages/Routes";
 import { useRouteMatch } from "react-router-dom";
+import { useMediaQuery } from "./hooks/useMediaQuery";
 
 const App = () => {
   let admin = useRouteMatch("/admin*");
   let login = useRouteMatch("*/login");
   let editor = useRouteMatch("/instance/editor/*");
+  const isSmallScreen = useMediaQuery("(max-width: 1024px)");
+
+  isSmallScreen
+    ? (window.location.href = "https://galaxieon.com/gcode.html")
+    : null;
 
   return (
     <>
